10c2e345a6590b90d49d5bc1e82856fc33e676e0
ms <msolce@gmail.com>
2022-11-13 18:34:26 -0300

    incluido variaveis de ambiente para o URL da API

--- src/utils/http-common.js
+++ src/utils/http-common.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-
+console.log(process.env)
 
 
 export default axios.create({
-    baseURL: `http://localhost:3001/api/v1` ,//process.env.BASE_URL,//'http://localhost:3001/api/v1', //http://localhost:3001
+    baseURL: process.env.REACT_APP_BASE_URL + '/api/v1',//process.env.BASE_URL,//'http://localhost:3001/api/v1', //http://localhost:3001
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json'
